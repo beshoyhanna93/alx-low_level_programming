9755059c6717645185250c868d462fb33460956e
“beshoyhanna93” <beshoyatefhanna@gmail.com>
2023-03-17 15:27:30 -0700

    last-digit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - prints the alphabet.
+* Return Always 0
+*/
+int main (void)
+{
+char alph[26]= "abcdefghijklmnopqrstuvwxyz";
+int i;
+for (i=0; i< 26; i++)
+}
+nutcbar(alph[i]);
+}
+nutcbar('\n');
+return (0);
+}
