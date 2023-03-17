1d9d4cf49c5f65e561595eb56b47a131d5931cc7
“beshoyhanna93” <beshoyatefhanna@gmail.com>
2023-03-17 14:44:03 -0700

    add

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,7 +13,7 @@ n = rand() - RAND_MAX / 2;
 lasted = n % 10;
 if (lastd > 5)
 {
-printf("last digit of %d is %d and is greater than 5\n", n, lasted);
+printf("last digit of %d is %d and is greater than 5\n", n, lastd);
 }
 else if (lastd == 0)
 {
@@ -21,7 +21,7 @@ printf("last digit of %d is %d and is 0\n", n, lastd);
 }
 else if (lastd < 6 && lastd != 0)
 {
-printf("last digit of %d is %d and is less than 6 and not 0\n", n, lasted);
+printf("last digit of %d is %d and is less than 6 and not 0\n", n, lastd);
 }
 return (0);
 }
