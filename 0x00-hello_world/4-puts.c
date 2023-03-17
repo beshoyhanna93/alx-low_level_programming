0666cfc12d7eb9ef7ac4c4dcb06262400df8d404
“beshoyhanna93” <beshoyatefhanna@gmail.com>
2023-03-16 18:19:50 -0700

    hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
-* main - main entry
-* Return always 0(success)
-*/
+ * main - this is main function
+ *
+ * Return: always 0
+ */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
+puts("\n"Programming is like building a multilingual puzzle);
 Return(0);
 }
