1f0d8635c38a9f598bcd5469352351177404f1b7
“beshoyhanna93” <beshoyatefhanna@gmail.com>
2023-03-16 19:18:58 -0700

    add4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
 /**
-*main -prints "program is like building a multilingual 
-*puzzle,followed by anew line."
-*Return:always 0
+* main - Entry point
+* Return always 0(success)
 */
 int main(void)
-{
-puts("\n'Programming is like building a multilingual puzzle");
-Return(0);
-}
+puts( \n"Programming is like building a multilingual puzzle");
+Returns(0);
