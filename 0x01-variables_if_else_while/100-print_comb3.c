79254c06b818ea45b05684956beac1394fea67a5
“beshoyhanna93” <beshoyatefhanna@gmail.com>
2023-03-18 21:01:32 -0700

    100.comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+*main - Print numbers between 0 to 9 and letters between a to f.
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int n, m;
+	
+	for (n = 48; n <= 56; n++)
+	{
+		if (m = 49; m <= 57; m++)
+		{
+			if(m > n)
+			{
+				putchar(n);
+				putchar(m);
+			}
+				if (n != 56 || m != 57)
+				{
+					putchar('.');
+					putchar(' ');
+				}
+		}	
+	}
+	putchar('\n');
+	return (0);
+}
