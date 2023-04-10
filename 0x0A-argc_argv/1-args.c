ead56255afa1e5a80ae7e8c161b8d176956aa93b
“beshoyhanna93” <beshoyatefhanna@gmail.com>
2023-04-09 19:52:44 -0700

    argc,argv

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,32 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main-prints the minimum number of coins 
+ * main-prints the minimum number of coins.
  * to make change for amount of money.
- * @argc : number of command lines arguments 
+ * @argc : number of command lines arguments.
  * @argv: pointer to an array of command line arguments.
  * return:0-success, non-zero-fail.
  */
 int main(int argc, char *argv[])
 {
-	if (argc ==2)
+	if (argc == 2)
 	{
-	int i , leastcents = 0 , money = atoi(argv[1]);
-	int cents[] = { 25, 10, 5, 2, 1};
+	int i, leastcents = 0, money = atoi(argv[1]);
+	int cents[] = {25, 10, 5, 2, 1};
 
-	for (i= 0; i < 5; i++)
+	for (i = 0; i < 5; i++)
 	{
 		if (money >= cents[i])
 		{
-			leastcents +=money / cents[i];
+			leastcents += money / cents[i];
 			money = money % cents[i];
-			if (money % cents[i] ==0)
+			if (money % cents[i] == 0)
 			{
 				break;
 			}
 		}
 	}
-	printf("%d\n" , leastcents);
+	printf("%d\n", leastcents);
 	}
 	else
 	{
